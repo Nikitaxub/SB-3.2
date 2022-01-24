ab37b0f2fa53275392b932ac18dc53a6fc2c0cf3
nikita <nikita.dmd@gmail.com>
2022-01-24 16:01:17 +0300

    Initial Commit

---
+++ RGBsliders/RGBslidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBslidersApp.swift
+//  RGBsliders
+//
+//  Created by xubuntus on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBslidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
