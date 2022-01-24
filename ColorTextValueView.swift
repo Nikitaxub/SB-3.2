54d14b6f413f5587f492c8896a11b98f71d5426a
nikita <nikita.dmd@gmail.com>
2022-01-24 19:31:59 +0300

    ready without keyboard

---
+++ ColorTextValueView.swift
@@ -0,0 +1,42 @@
+//
+//  ColorTextValueView.swift
+//  RGBsliders
+//
+//  Created by xubuntus on 24.01.2022.
+//
+
+import SwiftUI
+
+struct ColorTextValueView: View {
+    @Binding var value: Double
+    
+    var body: some View {
+        TextField("",
+                  text: $value.textValue
+                  )
+            .textFieldStyle(.roundedBorder)
+            .frame(width: 50)
+            .multilineTextAlignment(.trailing)
+    }
+}
+
+extension Double {
+    var textValue: String {
+        get { "\(Int(self))" }
+        set {
+            if newValue == "" {
+                self = 0
+            } else {
+                if let textValueRaw = Double(newValue) {
+                    self = textValueRaw > 255 ? 255 : textValueRaw
+                }
+            }
+        }
+    }
+}
+
+struct ColorTextValueView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorTextValueView(value: .constant(127))
+    }
+}
--- RGBsliders/ContentView.swift
+++ RGBsliders/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redValue: Double = 127
+    @State private var greenValue: Double = 127
+    @State private var blueValue: Double = 127
+    
+    private let width = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.orange)
+                .ignoresSafeArea()
+            VStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(.white, lineWidth: 3))
+                    .padding(.horizontal)
+                    .frame(height: width / 3)
+                .foregroundColor(Color(red: redValue / 255, green: greenValue / 255, blue: blueValue / 255))
+                .padding(.bottom, 50)
+                ColorValueView(value: $redValue, color: .red)
+                ColorValueView(value: $greenValue, color: .green)
+                ColorValueView(value: $blueValue, color: .blue)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorValueView: View {
+    @Binding var value: Double
+    let color: Color
+
+    var body: some View {
+        HStack {
+            Text(value.textValue)
+                .foregroundColor(.white)
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(color)
+            ColorTextValueView(value: $value)
+        }
+        .padding(.horizontal)
     }
 }
